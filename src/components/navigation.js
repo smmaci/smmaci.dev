95cda0e93c584099b134eab2781c03366c6d012e
Sam Mallamaci <sam.mallamaci@gmail.com>
2020-07-15 22:08:29 -0500

    Mobile-friendly + responsive navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,10 +6,12 @@ import GithubLogoPNG from './GitHub-Mark-32px.png'
 
 export const Navigation = () =>
     <nav class="Navigation">
+        <input type="checkbox" id="Navigation-toggle" />
+        <label for="Navigation-toggle" class="Navigation-toggleLabel">Menu</label>
         <ul class="Navigation-list">
             <li class="Navigation-listitem"><Link activeClassName="Navigation-link" class="Navigation-link" to="/">Home</Link></li>
             <li class="Navigation-listitem"><Link activeClassName="Navigation-link" class="Navigation-link" to="/about">About</Link></li>
             <li class="Navigation-listitem"><Link activeClassName="Navigation-link" class="Navigation-link" to="/side-projects">Side Projects</Link></li>
+            <li class="Navigation-listitem"><a href="https://github.com/smmaci"><img src={GithubLogoPNG} alt="The Github Logo"/></a></li>
         </ul>
-        <a href="https://github.com/smmaci"><img src={GithubLogoPNG} alt="The Github Logo"/></a>
     </nav>
