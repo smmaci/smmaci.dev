30a52af37c2a93e48469524c5b0b8711cb25651e
Stephanie <stephanie.mallamaci@gmail.com>
2022-11-07 20:54:18 -0600

    remove posts

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,6 @@ const { DateTime } = require("luxon");
 const pluginSEO = require("eleventy-plugin-seo");
 const Image = require("@11ty/eleventy-img")
 
-
-/**
-* This is the JavaScript code that determines the config for your Eleventy site
-*
-* You can add lost of customization here to define how the site builds your content
-* Try extending it to suit your needs!
-*/
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     // Templates:
@@ -66,34 +58,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLiquidShortcode("image", imageShortcode);
   eleventyConfig.addJavaScriptFunction("image", imageShortcode);
     // --- END, eleventy-img
-  
-
-  /* Build the collection of posts to list in the site
-     - Read the Next Steps post to learn how to extend this
-  */
-  eleventyConfig.addCollection("posts", function(collection) {
-    
-    /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
-       - https://www.11ty.dev/docs/collections/
-    */
-    
-    // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
-    // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
-    const coll = collection
-      .getFilteredByTag("posts");
-
-    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
-    // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
-    for (let i = 0; i < coll.length; i++) {
-      const prevPost = coll[i - 1];
-      const nextPost = coll[i + 1];
-
-      coll[i].data["prevPost"] = prevPost;
-      coll[i].data["nextPost"] = nextPost;
-    }
-
-    return coll;
-  });
 
   return {
     dir: {
